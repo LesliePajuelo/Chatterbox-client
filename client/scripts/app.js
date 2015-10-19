55ec0c5e5416bf571b781977cd57f96edfb3e05f
lesliepajuelo <l.l.pajuelo@gmail.com>
2015-10-19 14:32:10 -0700

    Add bootstrap

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,50 @@
 // YOUR CODE HERE:
 
+var dropDownBox = ("")
+
+var app = {
+  init: function () {
+  },
+  fetch: function (message) {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: undefined,
+      type: 'GET',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent. Data: ', data);
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message. Error: ', data);
+      }
+    });
+  },
+  send: function (message) {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent. Data: ', data);
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message. Error: ', data);
+      }
+    });
+  },
+  clearMessages: function () {
+  },
+  addMessage: function () {
+  },
+  handleSubmit: function () {
+  },
+  addRoom: function () {
+  },
+  addFriend: function () {
+  }
+};
